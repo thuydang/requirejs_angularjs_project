02f80c5b356a867b440123009b54c8933e25caca
Alba Mustafaj <albamustafaj8@gmail.com>
2017-07-03 15:08:54 +0200

    trying markers

--- auvegos.web.agent/src/main/webapp/app/components/tutorial/iscosimulation/simulation.controller.js
+++ auvegos.web.agent/src/main/webapp/app/components/tutorial/iscosimulation/simulation.controller.js
@@ -2,128 +2,121 @@ define([
     /* RequireJS modules definition files (w/o .js) */
     'app/app.module',
     'angular-wizard',
-		'angular-google-maps',
+    'angular-google-maps',
     //'bootstrap-dialog',
     'app/common/jsUtils/designPatterns',
     'app/components/maincontent/networkInfoServices'
 ], function (app) {
-    'use strict';
+    // 'use strict';
 
 
     app.register.controller('ServiceCtrl',
-			/* needed services */['$state','$scope', '$timeout', '$uibModal', '$log',
-                'EventAggregator', 'NetworkInfoWebsocketService', 'NetworkInfoRestServices', 'uiGmapGoogleMapApi',
-                function ($state, $scope, $timeout, $uibModal, $log,
+			/* needed services */['$state', '$scope', '$timeout', '$uibModal', '$log',
+            'EventAggregator', 'NetworkInfoWebsocketService', 'NetworkInfoRestServices', 'uiGmapGoogleMapApi',
+            function ($state, $scope, $timeout, $uibModal, $log,
                 EventAggregator, NetworkInfoWebsocketService, NetworkInfoRestServices, uiGmapGoogleMapApi) {
 
-               // Controller Logic
-							//
-							// GMAP Control
-							//
-									//
-							$scope.map = {center: {latitude: 52.1451, longitude: 13.6680 }, zoom: 12 };
-    //$scope.map = {center: {latitude: 40.1451, longitude: -99.6680 }, zoom: 4 };
-    $scope.options = {scrollwheel: false};
-    $scope.coordsUpdates = 0;
-    $scope.dynamicMoveCtr = 0;
-    $scope.marker = {
-      id: 0,
-      coords: {
-        latitude: 52.3451,
-        longitude: 13.3680
-      },
-      options: { draggable: true },
-      events: {
-        dragend: function (marker, eventName, args) {
-          $log.log('marker dragend');
-          var lat = marker.getPosition().lat();
-          var lon = marker.getPosition().lng();
-          $log.log(lat);
-          $log.log(lon);
-
-          $scope.marker.options = {
-            draggable: true,
-            labelContent: "latitude: " + $scope.marker.coords.latitude + ' ' + 'longitude: ' + $scope.marker.coords.longitude,
-            labelAnchor: "100 0",
-            labelClass: "marker-labels"
-          };
-        }
-      }
-    };
-    $scope.$watchCollection("marker.coords", function (newVal, oldVal) {
-      if (_.isEqual(newVal, oldVal))
-        return;
-      $scope.coordsUpdates++;
-    });
-    $timeout(function () {
-      $scope.marker.coords = {
-        latitude: 52.3051,
-        longitude: 13.3680
-      };
-      $scope.dynamicMoveCtr++;
-      $timeout(function () {
-        $scope.marker.coords = {
-          latitude: 52.3051,
-          longitude: 13.3680
-        };
-        $scope.dynamicMoveCtr++;
-      }, 2000);
-    }, 1000);		
-
-									//uiGmapGoogleMapApi.then(function(maps) {
-									//});
-									
-									// --
+                // Controller Logic
+                //
+                // GMAP Control
+                //
+                //
+                ///center at TU
+                $scope.map = {
+                    center: { latitude: 52.512230, longitude: 13.327135 },
+                    zoom: 14,
+                    onclick: {
+                        click: function (maps, eventName, arguments) {
+                            var e = arguments[0];
+                            $scope.lat = e.latLng.lat();
+                            $scope.lng = e.latLng.lng();
+                            console.log("my lat is:" + $scope.lat + ", lng is:" + $scope.lng);
+                        }
+                    }
+                };
+                $scope.options = { scrollwheel: true };
+                $scope.markers = [];
+                $scope.coordsUpdates = 0;
+                $scope.dynamicMoveCtr = 0;
+                
 
 
-									// 
-									// Directory Service Control
-									// 
-
-									//service to display in details
-									$scope.dispServ = "";
-
-									//type of services
-									$scope.setRandom = false;
-
-									//open the details' card
-									$scope.detailed = false;
-
-									//manual set locations
-									$scope.locations = [];
-
-									// List of services
-									$scope.serviceList = [
-										{
-											name: "Weather",
-											area: "Berlin",
-											sensor: ['Temperature', 'Humidity']
-										},
-										{
-											name: "Parking",
-											area: "BErl",
-											sensor: ['Bike', 'Car']
-										}
-									];
-
-									//array of temp changes
-									$scope.tempArr = $scope.serviceList.slice();
-
-									// service functions
-									$scope.getService = function (service) {
-										console.log(service);
-										$scope.detailed = true;
-										$scope.dispServ = service;
-										$scope.dispServ.sensor = service.sensor;
-
-									};
 
 
-              //  $scope.addService = function () {
-               //     $scope.detailed = true;
+                //uiGmapGoogleMapApi.then(function(maps) {
+                //});
+
+                // --
+
+
+                // 
+                // Directory Service Control
+                // 
+
+                //service to display in details
+                $scope.dispServ = "";
+
+                //type of services
+                $scope.setRandom = false;
+
+                //open the details' card
+                $scope.detailed = false;
+
+                //manual set locations
+                $scope.locations = [];
+
+                // List of services
+                $scope.serviceList = [
+                    {
+                        name: "Weather",
+                        area: "Berlin",
+                        sensor: ['Temperature', 'Humidity']
+                    },
+                    {
+                        name: "Parking",
+                        area: "BErl",
+                        sensor: ['Bike', 'Car']
+                    }
+                ];
+
+                //array of temp changes
+                $scope.tempArr = $scope.serviceList.slice();
+
+                // service functions
+                $scope.getService = function (service) {
+                    console.log(service);
+                    $scope.detailed = true;
+                    $scope.dispServ = service;
+                    $scope.dispServ.sensor = service.sensor;
+
+                };
+
+
+
+
+                $scope.setManual = function () {
+                    console.log("inside");
+                    var marker = new google.maps.Marker({
+                        position: {
+                            lat: parseFloat($scope.lat), lng: parseFloat($scope.lng)
+                        },
+                        id: Date.now(),
+                        setMap: $scope.map,
+                        title: "Alba is here"
+                    });
+
+                    $scope.markers.push(marker);
+                    console.log($scope.markers);
+                    
+
+                };
+
+
+                //  $scope.addService = function () {
+                //     $scope.detailed = true;
                 //};
 
-                
+
                 $scope.addServices = function () {
                     $scope.detailed = true;
                     $scope.tempArr.push({ 'name': $scope.search });
@@ -136,7 +129,7 @@ define([
                     var place = $scope.change;
                     $scope.details = true;
                     console.log(index);
-                    $scope.serviceList[index].i ="updat" ;
+                    $scope.serviceList[index].i = "updat";
                     console.log(updat);
 
                 };
@@ -159,14 +152,14 @@ define([
                     $scope.setRandom = true;
                     $scope.positions = [[52.5128, 13.3119], [52.5127, 13.3120], [52.5125, 13.3121]];
                 };
-               
-            
-              //  $scope.addProperty = function (index) {
-               //     $scope.serviceList[index].push({ '$scope.newProperty': 'Alba' });
-               // }
+
+
+                //  $scope.addProperty = function (index) {
+                //     $scope.serviceList[index].push({ '$scope.newProperty': 'Alba' });
+                // }
                 $scope.removeService = function (index) {
                     //can add alert later
-                   
+
 
                     var x = document.getElementById("my select");
                     x.remove(x.selectedIndex);
@@ -174,11 +167,11 @@ define([
                     //  console.log(index)
                 };
 
-               
-          
-                
-            // end of controller  */
 
-    }]);
+
+
+                // end of controller  */
+
+            }]);
 
 });
