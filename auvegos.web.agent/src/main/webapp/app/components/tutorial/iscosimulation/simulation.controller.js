60b88dbf90dd575fe002b04e0ca75b621524a752
Alba Mustafaj <albamustafaj8@gmail.com>
2017-07-04 15:59:20 +0200

    without random part

--- auvegos.web.agent/src/main/webapp/app/components/tutorial/iscosimulation/simulation.controller.js
+++ auvegos.web.agent/src/main/webapp/app/components/tutorial/iscosimulation/simulation.controller.js
@@ -11,10 +11,10 @@ define([
 
 
     app.register.controller('ServiceCtrl',
-			/* needed services */['$state', '$scope', '$timeout', '$uibModal', '$log',
-            'EventAggregator', 'NetworkInfoWebsocketService', 'NetworkInfoRestServices', 'uiGmapGoogleMapApi',
-            function ($state, $scope, $timeout, $uibModal, $log,
-                EventAggregator, NetworkInfoWebsocketService, NetworkInfoRestServices, uiGmapGoogleMapApi) {
+			/* needed services */['$state', '$scope', '$timeout', '$uibModal', '$log', '$mdDialog',
+                'EventAggregator', 'NetworkInfoWebsocketService', 'NetworkInfoRestServices', 'uiGmapGoogleMapApi', '$element',
+            function ($state, $scope, $timeout, $uibModal, $log, $mdDialog,
+                EventAggregator, NetworkInfoWebsocketService, NetworkInfoRestServices, uiGmapGoogleMapApi, $element) {
 
                 // Controller Logic
                 //
@@ -22,22 +22,66 @@ define([
                 //
                 //
                 ///center at TU
+
+                $scope.manual = false;
                 $scope.map = {
                     center: { latitude: 52.512230, longitude: 13.327135 },
                     zoom: 14,
-                    onclick: {
+                    markers: [],
+                    events: {
                         click: function (maps, eventName, arguments) {
-                            var e = arguments[0];
-                            $scope.lat = e.latLng.lat();
-                            $scope.lng = e.latLng.lng();
-                            console.log("my lat is:" + $scope.lat + ", lng is:" + $scope.lng);
+                            //
+                            if ($scope.manual === true) {
+                                var e = arguments[0];
+                                $scope.lat = e.latLng.lat();
+                                $scope.lng = e.latLng.lng();
+                                console.log("my lat is:" + $scope.lat + ", lng is:" + $scope.lng);
+                                var marker = {
+                                    id: Date.now(),
+                                    coords: {
+                                        latitude: $scope.lat,
+                                        longitude: $scope.lng
+                                    }
+                                };
+                                $scope.map.markers.push(marker);
+                                //put the selected area in the temporary service (which is being modified)
+                                $scope.dispServ.locations = $scope.map.markers;
+                                //see if markers are added in the array
+                                console.log($scope.dispServ.locations);
+                                $scope.$apply();
+                            }
+
+                            else if ($scope.setRandom === true) {
+                           //     var e = arguments[0];
+                             //   $scope.lat = e.latLng.lat();
+                              //  $scope.lng = e.latLng.lng();
+                                $scope.circle = new google.maps.Circle({
+                                    center: { latitude: 52.512230, longitude: 13.327135 },
+                              
+                                    radius: 500,
+                                    editable: true
+
+                                })
+
+                               // $scope.dispServ.area = cirlce;
+                                //put the selected area in the temporary service (which is being modified)
+                                console.log();
+                                //see if markers are added in the array
+                                
+                              //  $scope.$apply();
+                              
+
+
+
+                            }
+                            else {
+                                return;
+                            }
                         }
                     }
                 };
                 $scope.options = { scrollwheel: true };
-                $scope.markers = [];
-                $scope.coordsUpdates = 0;
-                $scope.dynamicMoveCtr = 0;
+               
                 
 
 
@@ -46,7 +90,7 @@ define([
                 //uiGmapGoogleMapApi.then(function(maps) {
                 //});
 
-                // --
+                // -------------------------------------------
 
 
                 // 
@@ -65,17 +109,28 @@ define([
                 //manual set locations
                 $scope.locations = [];
 
+
+                //service to visualize
+                $scope.searchTerm;
+
                 // List of services
                 $scope.serviceList = [
                     {
                         name: "Weather",
-                        area: "Berlin",
-                        sensor: ['Temperature', 'Humidity']
+                        area: {},
+                        sensor: ['Temperature', 'Humidity'],
+                        selectedSensor: "",
+                        selectedTypeOfSensor: "",
+                        locations: []
                     },
                     {
                         name: "Parking",
-                        area: "BErl",
-                        sensor: ['Bike', 'Car']
+                        area: {},
+                        sensor: ['Bike', 'Car'],
+                        selectedSensor: "",
+                        selectedTypeOfSensor:"",
+                        locations: []
+
                     }
                 ];
 
@@ -83,80 +138,86 @@ define([
                 $scope.tempArr = $scope.serviceList.slice();
 
                 // service functions
+
+                //when a service is selected
+                //assign the existing service parameters to the temporary one (which is being modified)
                 $scope.getService = function (service) {
                     console.log(service);
                     $scope.detailed = true;
                     $scope.dispServ = service;
                     $scope.dispServ.sensor = service.sensor;
-
-                };
-
-
-
-
-                $scope.setManual = function () {
-                    console.log("inside");
-                    var marker = new google.maps.Marker({
-                        position: {
-                            lat: parseFloat($scope.lat), lng: parseFloat($scope.lng)
-                        },
-                        id: Date.now(),
-                        setMap: $scope.map,
-                        title: "Alba is here"
-                    });
-
-                    $scope.markers.push(marker);
-                    console.log($scope.markers);
                     
-
                 };
 
+                //decide the sensor
+                $scope.selectSensor = function (item) {
+                    $scope.dispServ.selectedSensor = item;
+                    console.log($scope.dispServ.selectedSensor);
+                }
+                //allow manual selection
+                $scope.setManual = function () {
 
-                //  $scope.addService = function () {
-                //     $scope.detailed = true;
-                //};
+                    console.log("inside");
+                    $scope.dispServ.selectedTypeOfSensor = "Manual";
+                    $scope.manual = true;
+                    console.log($scope.dispServ.selectedTypeOfSensor);
+                    
+                };
 
-
+                //add new service
                 $scope.addServices = function () {
                     $scope.detailed = true;
                     $scope.tempArr.push({ 'name': $scope.search });
                     $scope.search = '';
                     console.log($scope.tempArr);
+                    //check if it is added to the real service list
                     console.log($scope.serviceList);
                 };
 
-                $scope.editService = function ($index, i, updat) {
-                    var place = $scope.change;
-                    $scope.details = true;
-                    console.log(index);
-                    $scope.serviceList[index].i = "updat";
-                    console.log(updat);
 
-                };
-
+                //when save button is pressed
                 $scope.saveChanges = function () {
                     $scope.serviceList = $scope.tempArr;
-
+                    //check if locations are saved
+                    console.log($scope.serviceList[0].locations);
+                    console.log($scope.serviceList[0].selectedSensor);
                 };
 
-
+                //when cancel button is pressed
                 $scope.cancelAll = function () {
                     $scope.tempArr = $scope.serviceList;
                 };
 
-                $scope.randomize = function () {
+
+                $scope.makeRandom = function () {
                     $scope.setRandom = true;
+                    $scope.dispServ.selectedTypeOfSensor = "Random";
+
+
                 };
 
+                $scope.visualize = function (service) {
+
+
+                };
+
+                //allow random generation (TODO)
                 $scope.randomPos = function (event) {
                     $scope.setRandom = true;
                     $scope.positions = [[52.5128, 13.3119], [52.5127, 13.3120], [52.5125, 13.3121]];
                 };
 
+                
+                $scope.clearSearchTerm = function () {
+                    $scope.searchTerm = '';
+                };
+                // The md-select directive eats keydown events for some quick select
+                // logic. Since we have a search input here, we don't need that logic.
+              //  $element.find('input').on('keydown', function (ev) {
+              //      ev.stopPropagation();
+               // });
 
-                //  $scope.addProperty = function (index) {
-                //     $scope.serviceList[index].push({ '$scope.newProperty': 'Alba' });
-                // }
+                //remove service (still not decided)
                 $scope.removeService = function (index) {
                     //can add alert later
 
@@ -174,4 +235,53 @@ define([
 
             }]);
 
-});
+    });
+
+
+/*function displayLocation(latitude, longitude) {
+    var request = new XMLHttpRequest();
+
+    var method = 'GET';
+    var url = 'http://maps.googleapis.com/maps/api/geocode/json?latlng='
+        + latitude + ',' + longitude + '&sensor=true';
+    var async = true;
+
+    request.open(method, url, async);
+    request.onreadystatechange = function () {
+        if (request.readyState == 4 && request.status == 200) {
+            var data = JSON.parse(request.responseText);
+            var address = data.results[0];
+            var value = address.formatted_address.split(",");
+            count = value.length;
+            country = value[count - 1];
+            state = value[count - 2];
+            city = value[count - 3];
+            alert(city);
+            // document.write(address.formatted_address);
+
+        }
+    };
+    request.send();
+};
+
+var successCallback = function (position) {
+    var x = position.coords.latitude;
+    var y = position.coords.longitude;
+    displayLocation(x, y);
+};
+
+var errorCallback = function (error) {
+
+};
+
+var options = {
+    enableHighAccuracy: true,
+    timeout: 1000,
+    maximumAge: 0
+};
+
+navigator.geolocation.getCurrentPosition(successCallback, errorCallback,
+    options);
+
+
+*/
